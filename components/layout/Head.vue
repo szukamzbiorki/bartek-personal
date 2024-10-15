eb2843057bddb925547a8a5abd2a03ea155d258b
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-10-15 13:48:57 +0200

    ta

--- components/layout/Head.vue
+++ components/layout/Head.vue
@@ -2,9 +2,7 @@
 	<header>
 		<div class="title">
 			<div class="label">Bartek Pierściński (2001, Kraków)</div>
-			<div class="content">
-				Designer and developer based in Den Haag, The Netherlands
-			</div>
+			<div class="content">Designer and developer based in Den Haag, The Netherlands</div>
 			<!-- <div class="rest">
 				2020-2024 BA Graphic Design, Royal Academy of Art, Den Haag
 			</div> -->
@@ -16,15 +14,15 @@
 				<a href="https://turnus.fun">turnus.fun</a>
 				<!-- <a href="https://roots.kabk.nl/a">roots.kabk.nl</a> -->
 				<a href="https://julesjanssen.biz">julesjanssen.biz</a>
+				<a href="https://0000.garden">0000.garden</a>
+				<a href="https://kabk.github.io/go-theses-24/">kabk.github.io [2024 dg theses]</a>
 			</div>
 		</div>
 		<div class="contact">
 			<div class="label">Contact</div>
 			<div class="content">
 				<a href="mailto:contact@bartekp.info">contact@bartekp.info</a>
-				<a href="https://instagram.com/bartekpierscinski"
-					>(at)bartekpierscinski</a
-				>
+				<a href="https://instagram.com/bartekpierscinski">(at)bartekpierscinski</a>
 			</div>
 		</div>
 		<div class="bat">
